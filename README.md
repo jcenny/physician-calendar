8772ae7e35c127fc3c646233f0fe7d1c873f51c3
jcenny <jcenny26@gmail.com>
2020-11-10 11:16:34 -0800

    Initial directory and webpack setup

---
+++ README.md
@@ -0,0 +1,25 @@
+# Notable
+
+> Project description
+
+## Table of Contents
+
+1. [Development](#development)
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+npm start
+```
+
+To make changes and recompile webpack bundle:
+
+```sh
+npm run build
+```
---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening from port: ${port}`))
