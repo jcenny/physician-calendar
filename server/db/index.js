8269902e0e68a27e4b93fd029ece07be642a005d
jcenny <jcenny26@gmail.com>
2020-11-18 14:39:11 -0800

    Deleted database connection/schema

--- server/db/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb://localhost/physiciancalendar';
-
-const db = mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-module.exports = db;
-
